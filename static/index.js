c90242f09b26f46732db5a719db4010f88e2c1ec
FireStreaker2 <103970465+FireStreaker2@users.noreply.github.com>
2023-04-25 20:39:02 -0700

    graphics

--- static/index.js
+++ static/index.js
@@ -1,15 +1,7 @@
 const input = document.getElementById("input");
 const output = document.getElementById("output");
 
-output.textContent = "fetch(\"http://addapi.firestreaker2.gq/add?x=1&y=1\", {\n" +
-    "  method: \"GET\",\n" +
-    "  headers: {\n" +
-    "    \"Content-Type\": \"application/json\"\n" +
-    "  }\n" +
-    "})\n" +
-    "  .then(response => response.json())\n" +
-    "  .then(data => console.log(data))\n" +
-    "  .catch(error => console.error(error));";
+output.innerHTML = "<img class='img' src='javascript.png' />";
 
 input.addEventListener("change", () => {
     var language = input.value;
@@ -17,29 +9,15 @@ input.addEventListener("change", () => {
 
     switch (language) {
         case "javascript":
-            output.textContent = "fetch(\"http://addapi.firestreaker2.gq/add?x=1&y=1\", {\n" +
-                "  method: \"GET\",\n" +
-                "  headers: {\n" +
-                "    \"Content-Type\": \"application/json\"\n" +
-                "  }\n" +
-                "})\n" +
-                "  .then(response => response.json())\n" +
-                "  .then(data => console.log(data))\n" +
-                "  .catch(error => console.error(error));";
+            output.innerHTML = "<img class='img' src='javascript.png' />";
             break;
 
         case "python":
-            output.textContent = "import requests\n\n" +
-                "response = requests.get(\"http://addapi.firestreaker2.gq/add?x=1&y=1\")\n" +
-                "if response.status_code == 200:\n" +
-                "  data = response.json()\n" +
-                "  print(data)\n" +
-                "else:\n" +
-                "  print(\"Error: {}\".format(response.status_code))";
+            output.innerHTML = "<img class='img' src='python.png' />";
             break;
 
         case "curl":
-            output.textContent = "curl -X GET \"http://addapi.firestreaker2.gq/add?x=1&y=1\"";
+            output.innerHTML = "<img class='img' src='terminal.png' />";
             break;
 
         default:
